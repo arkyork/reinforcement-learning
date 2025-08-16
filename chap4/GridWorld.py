cecb64401914df874964bac7739b982113d08540
unknown <abcvfga123123@gmail.com>
2025-08-16 18:09:37 +0900

    add:greedy

--- chap4/GridWorld.py
+++ chap4/GridWorld.py
@@ -1,5 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import defaultdict
+
+def argmax(action_values):
+    max_value = max(action_values.values())
+    max_key = 0
+    for key,values in action_values.items():
+        if values == max_value :
+            max_key = key
+            break
+    return max_key
 
 class GridWorld:
     def __init__(self):
@@ -64,7 +74,7 @@ class GridWorld:
         next_x ,next_y = next_state
 
         # 収まっていなければ現在の位置へ
-        if (next_x < 0 or next_x >= self.width) or  (next_y < 0 or next_x >= self.height):
+        if (next_x < 0 or next_x >= self.height) or (next_y < 0 or next_y >= self.width):
             next_state = state
         elif next_state == self.obstacle_state:
             next_state = state
@@ -88,9 +98,10 @@ class GridWorld:
                 next_state = env.next_state(state,action)
                 r = env.reward(state,action,next_state)
 
-                new_V += action_prob +(r + gamma * V[next_state])
+                new_V += action_prob * (r + gamma * V[next_state])
             V[state] = new_V
-
+        return V
+    
     # 価値関数の可視化
     def render_value_function(self, V):
         # Vは {(i,j): value} の辞書を想定
@@ -123,3 +134,59 @@ class GridWorld:
         plt.colorbar(label="Value")
         plt.title("Value Function Heatmap")
         plt.show()
+    def policy_eval(self,pi,V,env,gamma ,thershold = 0.001):
+        while True:
+            old_V = V.copy()
+            V = self.eval_one(pi,V,env,gamma)
+
+            delta = 0
+
+            for state in V.keys():
+                temp = abs(V[state] -old_V[state])
+                if delta < temp:
+                    delta = temp
+
+            if delta < thershold:
+                break
+        return V
+    def greedy_policy(self,V,env,gamma):
+        # 方策
+        pi = {}
+
+        for state in self.states():
+
+            action_values = {}
+
+            for action in self.actions():
+
+                next_state = env.next_state(state,action)
+                r = env.reward(state,action,next_state)
+                value = r + gamma * V[next_state]
+                action_values[action] = value
+            
+            # 最大の行動
+            max_action = argmax(action_values)
+            action_probs = { i:0 for i in range(4)}
+            # 決定論的に変換
+            action_probs[max_action] = 1
+            
+            pi[state] = action_probs
+        return pi
+    # greedy_policyを繰り返す
+    def greedy_itr(self,env,gamma,thershold = 0.001):
+        # 方策
+        pi = defaultdict(lambda : { i:0.25 for i in range(4)})
+        # 価値関数
+        V = defaultdict(lambda : 0)
+
+        while True:
+            V = self.policy_eval(pi,V,env,gamma,thershold)
+            new_pi = self.greedy_policy(V,env,gamma)
+
+            if new_pi == pi:
+                break
+            
+            pi = new_pi
+        return pi
+    
+    
